e572fcc7d04c57eecbadde19b7cdc8ef325c1061
akremwahabi13 <akremwahabi3@gmail.com>
2023-03-27 23:06:42 +0200

    commit

---
+++ src/Components/Profile/Adress.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Adress() {
+  return (
+    <div>
+      {" "}
+      <h1 className="title">Adress : Ksar Bardo City Beja</h1>
+    </div>
+  );
+}
+
+export default Adress
---
+++ src/Components/Profile/FullName.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Adress from './Adress';
+
+function FullName() {
+  return (
+    <div>
+      <h1 className="title">Akrem Wahebi</h1>
+      <Adress/>
+    </div>
+  );
+}
+
+export default FullName
